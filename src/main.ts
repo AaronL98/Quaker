066a8916e7e8ac721d4df02f11af136bd6698866
AaronL98-continuum <me@aaron.dog>
2025-03-20 16:54:50 +0000

    Reconfigure primevue theming

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,35 @@ import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 const app = createApp(App);
 app.use(router);
+
+const QuakerPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}',
+    },
+  },
+});
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: QuakerPreset,
+    options: {
+      darkModeSelector: '.dark-mode',
+    },
   },
 });
 app.mount('#app');
