9f1bc92098e172e1d5cf97209f62a79b26050182
AaronL98-continuum <me@aaron.dog>
2025-03-20 16:47:40 +0000

    Set up primevue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import Button from 'primevue/button';
+</script>
+
 <template>
-  <div class="text-3xl font-bold">Quaker</div>
+  <div class="flex flex-col items-center h-screen justify-center">
+    <span class="font-bold text-3xl mb-4">Quaker - Styled by Tailwind</span>
+    <Button label="My PrimeVue button" />
+  </div>
 </template>
