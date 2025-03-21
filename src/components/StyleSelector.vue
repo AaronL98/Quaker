466f010f6639c8e1aae6ca728db94d1973647089
AaronL98-continuum <me@aaron.dog>
2025-03-21 01:53:46 +0000

    Style selector component WIP

--- src/components/StyleSelector.vue
+++ src/components/StyleSelector.vue
@@ -1,9 +1,77 @@
 <script lang="ts" setup>
+import { ref, computed } from 'vue';
 import Button from 'primevue/button';
+import OverlayBadge from 'primevue/overlaybadge';
+import Popover from 'primevue/popover';
+import { useMapStore } from '@/stores/mapStore';
+
+import { MAP_STYLES_LIST } from '@/consts/mapStyles.types';
+
+import type { LngLat } from 'mapbox-gl';
+
+const mapStore = useMapStore();
+
+import { getMapStyleThumbnail } from '@/helpers/getMapStyleThumbnail';
+
+const popover = ref();
+
+const toggle = (event: any) => {
+  popover.value.toggle(event);
+};
+
+const accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
+console.log('accessToken', accessToken);
+
+const getThumbnail = (styleUsername: string, styleId: string) => {
+  const center: LngLat = mapStore.getCenter;
+  const zoom: number = mapStore.getZoom;
+
+  return getMapStyleThumbnail(styleUsername, styleId, center.lng, center.lat, zoom, accessToken);
+  // return getMapStyleThumbnail('test', accessToken);
+};
+
+const activeStyleName = computed(() => {
+  console.log('');
+  return mapStore.getStyleName;
+});
+
+const setStyle = async (styleUrl: string) => {
+  await mapStore.setStyle(styleUrl);
+
+  console.log('new active style', activeStyleName.value);
+};
 </script>
 
 <template>
   <div>
-    <Button icon="mdi mdi-layers" class="shadow-xl" rounded size="large" severity="secondary" />
+    <Button
+      @click="toggle"
+      icon="mdi mdi-layers"
+      class="shadow-xl"
+      rounded
+      size="large"
+      severity="secondary" />
+
+    <Popover ref="popover">
+      <div class="font-bold mb-2">Style selector</div>
+      <div class="flex gap-4">
+        <OverlayBadge
+          v-for="style in MAP_STYLES_LIST"
+          value="✓"
+          :pt="{
+            pcBadge: {
+              root: {
+                class: activeStyleName === style.name ? '' : '!hidden',
+              },
+            },
+          }">
+          <img
+            @click="setStyle(style.url)"
+            :src="getThumbnail(style.username, style.id)"
+            class="hover:border-primary-400 border-surface-800 border-1 cursor-pointer"
+            :alt="style.name" />
+        </OverlayBadge>
+      </div>
+    </Popover>
   </div>
 </template>
