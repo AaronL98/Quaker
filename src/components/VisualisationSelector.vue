09fa3ae56d500e65b858f6039fbbcfb326f34291
AaronL98-continuum <me@aaron.dog>
2025-03-22 23:05:01 +0000

    Visualisation selector start of component

---
+++ src/components/VisualisationSelector.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Button from 'primevue/button';
+import Popover from 'primevue/popover';
+import Listbox from 'primevue/listbox';
+
+const popover = ref();
+
+const toggle = (event: any) => {
+  popover.value.toggle(event);
+};
+</script>
+<template>
+  <div>
+    <Button
+      v-tooltip.left="'Select a visualisation'"
+      @click="toggle"
+      icon="mdi mdi-chart-scatter-plot"
+      class="shadow-xl"
+      size="large"
+      severity="primary"
+      rounded />
+
+    <Popover ref="popover">
+      <span class="block font-medium mb-2">Visualisation selector </span>
+    </Popover>
+  </div>
+</template>
