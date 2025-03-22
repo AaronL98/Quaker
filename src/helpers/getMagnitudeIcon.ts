2a97215b078ba3ce6172f311b525b70cd442307a
AaronL98-continuum <me@aaron.dog>
2025-03-22 01:04:25 +0000

    Magnitude icon generator

---
+++ src/helpers/getMagnitudeIcon.ts
@@ -0,0 +1,11 @@
+//A function that takes an earthquake magnitude value and returns a corresponding cellular signal icon and colour
+
+export const getMagnitudeIcon = (magnitude: number) => {
+  if (magnitude < 3) {
+    return 'mdi-signal-cellular-1 text-green-700 dark:text-green-300';
+  } else if (magnitude < 5) {
+    return 'mdi-signal-cellular-2 text-yellow-700 dark:text-yellow-300';
+  } else {
+    return 'mdi-signal-cellular-3 text-red-700 dark:text-red-300';
+  }
+};
