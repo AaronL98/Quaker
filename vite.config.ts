122b94627244d91a0d0294780ba17713fc546dc1
AaronL98-continuum <me@aaron.dog>
2025-03-23 20:49:50 +0000

    case sensitive with repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quaker/',
+  base: '/Quaker/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
