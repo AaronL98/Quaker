c03c7db6ef9434577230c5243f75794b23e79c42
AaronL98-continuum <me@aaron.dog>
2025-03-24 00:27:52 +0000

    Remove redundant code for fetching from static api

--- src/consts/mapStyles.types.ts
+++ src/consts/mapStyles.types.ts
@@ -1,3 +1,8 @@
+import streetsPreview from '@/assets/mapPreviews/streets-v12.png';
+import lightPreview from '@/assets/mapPreviews/light-v10.png';
+import darkPreview from '@/assets/mapPreviews/dark-v10.png';
+import satellitePreview from '@/assets/mapPreviews/satellite-v9.png';
+
 export interface MapStyle {
   name: string;
   url: string;
@@ -9,24 +14,28 @@ export const MAP_STYLES = {
     url: 'mapbox://styles/mapbox/streets-v12',
     username: 'mapbox',
     id: 'streets-v12',
+    img: streetsPreview,
   },
   LIGHT: {
     name: 'Mapbox Light',
     url: 'mapbox://styles/mapbox/light-v10',
     username: 'mapbox',
     id: 'light-v10',
+    img: lightPreview,
   },
   DARK: {
     name: 'Mapbox Dark',
     url: 'mapbox://styles/mapbox/dark-v10',
     username: 'mapbox',
     id: 'dark-v10',
+    img: darkPreview,
   },
   SATELLITE: {
     name: 'Mapbox Satellite',
     url: 'mapbox://styles/mapbox/satellite-v9',
     username: 'mapbox',
     id: 'satellite-v9',
+    img: satellitePreview,
   },
 } as const;
 
